7bdbed26ef28678101127e8a857b5ab720ff8e1b
Pavel Zwerschke <pavelzw@gmail.com>
2021-06-13 18:48:08 +0200

    Add support for multiple tasks

--- pywatts_sqf_rnn.py
+++ pywatts_sqf_rnn.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from itertools import product
 import xarray as xr
+import numpy as np
 from gluonts.model.deepar import DeepAREstimator
 from gluonts.mx.distribution import PiecewiseLinearOutput
 from gluonts.mx.trainer import Trainer
@@ -8,7 +9,6 @@ from gluonts.mx.trainer import Trainer
 from gluonts_wrapper import GluonTSWrapper
 from multi_dim_csv_callback import MultiDimCSVCallback
 from number_callback import NumberCallback
-from pywatts.callbacks import CSVCallback
 from pywatts.core.pipeline import Pipeline
 from pywatts.wrapper import FunctionModule
 
@@ -89,71 +89,75 @@ def pinball_loss(actual, prediction):
 
 
 if __name__ == '__main__':
-    task = 6
-    import numpy as np
-    predictor_zones, train_data_zones, gefcom14_metadata = load_data()
+    results = pd.DataFrame(columns=range(4, 16), index=['loss'])
 
-    zone = 3
-    x_input1 = xr.DataArray(predictor_zones[0])
-    x_input2 = xr.DataArray(predictor_zones[1])
-    x_input3 = xr.DataArray(predictor_zones[2])
-    x_input = xr.DataArray(coords=[('TIMESTAMP', x_input1.TIMESTAMP.data),
-                                   ('ZONE', [1, 2, 3]),
-                                   ('dim_0', x_input1.dim_1.data)])
-    x_input[:, 0, :] = x_input1
-    x_input[:, 1, :] = x_input2
-    x_input[:, 2, :] = x_input3
+    for task in range(4, 16):
+        print(f'Task {task}')
 
-    y_output1 = xr.DataArray(train_data_zones[0])
-    y_output2 = xr.DataArray(train_data_zones[1])
-    y_output3 = xr.DataArray(train_data_zones[2])
-    y_output = xr.DataArray(coords=[('TIMESTAMP', y_output1.TIMESTAMP.data),
-                                   ('ZONE', [1, 2, 3]),
-                                   ('dim_0', y_output1.dim_1.data)])
-    y_output[:, 0, :] = y_output1
-    y_output[:, 1, :] = y_output2
-    y_output[:, 2, :] = y_output3
+        predictor_zones, train_data_zones, gefcom14_metadata = load_data()
 
-    data_train = {'x_input': x_input[:-gefcom14_metadata['prediction_length'], :, :],
-                  'y_output': y_output[:-gefcom14_metadata['prediction_length'], :, :]}
-    data_test = {'x_input': x_input,
-                 'y_output': y_output}
+        x_input1 = xr.DataArray(predictor_zones[0])
+        x_input2 = xr.DataArray(predictor_zones[1])
+        x_input3 = xr.DataArray(predictor_zones[2])
+        x_input = xr.DataArray(coords=[('TIMESTAMP', x_input1.TIMESTAMP.data),
+                                       ('ZONE', [1, 2, 3]),
+                                       ('dim_0', x_input1.dim_1.data)])
+        x_input[:, 0, :] = x_input1
+        x_input[:, 1, :] = x_input2
+        x_input[:, 2, :] = x_input3
 
-    pipeline_train = Pipeline(path='results/sqf-rnn/test')
+        y_output1 = xr.DataArray(train_data_zones[0])
+        y_output2 = xr.DataArray(train_data_zones[1])
+        y_output3 = xr.DataArray(train_data_zones[2])
+        y_output = xr.DataArray(coords=[('TIMESTAMP', y_output1.TIMESTAMP.data),
+                                       ('ZONE', [1, 2, 3]),
+                                       ('dim_0', y_output1.dim_1.data)])
+        y_output[:, 0, :] = y_output1
+        y_output[:, 1, :] = y_output2
+        y_output[:, 2, :] = y_output3
 
-    neural_network = GluonTSWrapper(module=DeepAREstimator(
-        freq=gefcom14_metadata['freq'],
-        prediction_length=gefcom14_metadata['prediction_length'],
-        distr_output=PiecewiseLinearOutput(num_pieces=3),  # SQF-RNN
-        use_feat_dynamic_real=True,
-        trainer=Trainer(epochs=1)
-        # trainer=Trainer(epochs=7)
-    ),  start=gefcom14_metadata['start'],
-        freq=gefcom14_metadata['freq'],
-        name='neural network')
+        data_train = {'x_input': x_input[:-gefcom14_metadata['prediction_length'], :, :],
+                      'y_output': y_output[:-gefcom14_metadata['prediction_length'], :, :]}
+        data_test = {'x_input': x_input,
+                     'y_output': y_output}
 
-    nn_output_train = neural_network(
-        x=pipeline_train['x_input'], target=pipeline_train['y_output']
-    )
+        pipeline_train = Pipeline(path='results/sqf-rnn/test')
 
-    print('Training SQF-RNN model...')
-    pipeline_train.train(data_train)
-    print('Training finished.')
+        neural_network = GluonTSWrapper(module=DeepAREstimator(
+            freq=gefcom14_metadata['freq'],
+            prediction_length=gefcom14_metadata['prediction_length'],
+            distr_output=PiecewiseLinearOutput(num_pieces=3), # SQF-RNN
+            use_feat_dynamic_real=True,
+            trainer=Trainer(epochs=7)
+        ),  start=gefcom14_metadata['start'],
+            freq=gefcom14_metadata['freq'],
+            name='neural network')
 
-    # testing
-    pipeline_test = Pipeline(path='results/sqf-rnn/test')
+        nn_output_train = neural_network(
+            x=pipeline_train['x_input'], target=pipeline_train['y_output']
+        )
 
-    nn_output_test = neural_network(
-        x=pipeline_test['x_input'],
-        callbacks=[MultiDimCSVCallback(f'task{task} zone{zone}')]
-    )
+        print('Training SQF-RNN model...')
+        pipeline_train.train(data_train)
+        print('Training finished.')
 
-    pinball_loss_evaluated = FunctionModule(name='pinball loss', transform_method=pinball_loss)(
-        actual=pipeline_test['y_output'], prediction=nn_output_test,
-        callbacks=[NumberCallback(f'task{task} zone{zone}', (0,))]
-    )
+        # testing
+        pipeline_test = Pipeline(path='results/sqf-rnn/test')
 
-    output = pipeline_test.test(data_test)
-    loss = output['pinball loss'].data[0]
-    print(loss)
+        nn_output_test = neural_network(
+            x=pipeline_test['x_input'],
+            callbacks=[MultiDimCSVCallback(f'task{task}')]
+        )
 
+        pinball_loss_evaluated = FunctionModule(name='pinball loss', transform_method=pinball_loss)(
+            actual=pipeline_test['y_output'], prediction=nn_output_test,
+            callbacks=[NumberCallback(f'task{task}', (0,))]
+        )
+
+        output = pipeline_test.test(data_test)
+        loss = round(output['pinball loss'].data[0], 5)
+        results[task] = loss
+        print(f'---------------------------\nLoss for task {task}: {loss}\n'
+              f'---------------------------')
+    print(results)
+    results.to_csv('results/sqf_rnn/test/losses.csv')
